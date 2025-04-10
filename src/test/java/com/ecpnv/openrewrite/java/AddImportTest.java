78380a520e34a8ae3f439ea266df91464710ab4d
Wouter Veltmaat <wouter.veltmaat@opencircle.solutions>
2025-04-10 17:03:04 +0200

    JDOJPA-124: - fixed edge case for inner class import

--- src/test/java/com/ecpnv/openrewrite/java/AddImportTest.java
+++ src/test/java/com/ecpnv/openrewrite/java/AddImportTest.java
@@ -7,6 +7,8 @@ import static org.openrewrite.java.Assertions.java;
 
 import com.ecpnv.openrewrite.jdo2jpa.BaseRewriteTest;
 
+import org.openrewrite.test.SourceSpec;
+
 class AddImportTest extends BaseRewriteTest {
 
     @DocumentExample
@@ -33,5 +35,35 @@ class AddImportTest extends BaseRewriteTest {
                             private List<String> names;
                         }
                         """));
+    }
+
+    @DocumentExample
+    @Test
+    void innerClass() {
+        rewriteRun(spec -> spec.parser(PARSER)
+                        .recipeFromResources("com.ecpnv.openrewrite.jdo2jpa.v2x.post.causeway")
+                ,
+                //language=java
+                java("""
+                        package org.incode.module.communications;
+                        
+                        public interface EstatioSharedKernelCommunicationsModule {
+                            abstract class AbstractEvent{}
+                        }
+                        """, SourceSpec::skip),
+                java(
+                        """
+                                package org.incode.module.communications.integtests.dom.communications.dom.demowithnotes;
+                                
+                                import org.springframework.context.annotation.Configuration;
+                                import org.incode.module.communications.EstatioSharedKernelCommunicationsModule;
+                                
+                                @Configuration
+                                public class DemoObjectWithNotes_invoices {
+                                    public abstract class SomeEvent extends EstatioSharedKernelCommunicationsModule.AbstractEvent {}
+                                }
+                                """
+                )
+        );
     }
 }
