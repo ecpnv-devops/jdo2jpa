890a48de1883f3bb0d6cee8bba55221cb5da81d3
Wouter Veltmaat <wouter.veltmaat@opencircle.solutions>
2025-06-23 14:06:49 +0200

    JDOJPA-148: add replace class annotation recipe for a specific use-case because a openrewrite default recipe didn't work as expected PtV

--- src/test/java/com/ecpnv/openrewrite/jdo2jpa/ReplaceClassAnnotationTest.java
+++ src/test/java/com/ecpnv/openrewrite/jdo2jpa/ReplaceClassAnnotationTest.java
@@ -119,4 +119,70 @@ public class ReplaceClassAnnotationTest extends BaseRewriteTest {
                 )
         );
     }
+
+    @DocumentExample
+    @Test
+    void happyPath3() {
+        rewriteRun(
+                spec -> spec.parser(PARSER)
+                        .recipes(new ReplaceClassAnnotation(
+                                "@EntityListeners(org.apache.isis.persistence.jpa.applib.integration.IsisEntityListener.class)",
+                                "@EntityListeners(OrmEntityListener.class)",
+                                "org.estatio.base.prod.integration.OrmEntityListener")),
+                java("""
+                                        package org.estatio.base.prod.integration;
+                                
+                                        public class OrmEntityListener {
+                                        }
+                                """,
+                        SourceSpec::skip),
+                java(
+                        """
+                                package org.estatio.base.prod.util.dom;
+                                
+                                import lombok.AllArgsConstructor;
+                                import lombok.NoArgsConstructor;
+                                
+                                import javax.persistence.Entity;
+                                import javax.persistence.EntityListeners;
+                                import javax.persistence.Id;
+                                
+                                @Entity
+                                @EntityListeners(org.apache.isis.persistence.jpa.applib.integration.IsisEntityListener.class)
+                                @NoArgsConstructor
+                                @AllArgsConstructor
+                                public class SomeEntity {
+                                
+                                    @Id
+                                    private Long id;
+                                
+                                }
+                                """
+                        ,
+                        """
+                                package org.estatio.base.prod.util.dom;
+                                
+                                import lombok.AllArgsConstructor;
+                                import lombok.NoArgsConstructor;
+                                import org.estatio.base.prod.integration.OrmEntityListener;
+                                
+                                import javax.persistence.Entity;
+                                import javax.persistence.EntityListeners;
+                                import javax.persistence.Id;
+                                
+                                @Entity
+                                @EntityListeners(OrmEntityListener.class)
+                                @NoArgsConstructor
+                                @AllArgsConstructor
+                                public class SomeEntity {
+                                
+                                    @Id
+                                    private Long id;
+                                
+                                }
+                                """
+                )
+        );
+    }
+
 }
