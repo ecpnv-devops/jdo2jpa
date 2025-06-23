fd16fcd592cfad618e1a5c676889eb3cae3fa99b
Wouter Veltmaat <wouter.veltmaat@opencircle.solutions>
2025-06-23 14:07:17 +0200

    JDOJPA-148: add replace class annotation recipe for a specific use-case because a openrewrite default recipe didn't work as expected PtVI

--- src/test/java/com/ecpnv/openrewrite/jdo2jpa/ReplaceClassAnnotationTest.java
+++ src/test/java/com/ecpnv/openrewrite/jdo2jpa/ReplaceClassAnnotationTest.java
@@ -158,7 +158,7 @@ public class ReplaceClassAnnotationTest extends BaseRewriteTest {
                                 
                                 }
                                 """
-                        ,
+                ,
                         """
                                 package org.estatio.base.prod.util.dom;
                                 
