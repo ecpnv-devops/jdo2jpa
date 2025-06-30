4357891c8703da2431ebc247c91335f4b66a9fca
Patrick Deenen <patrick.deenen@opencirclesolutions.nl>
2025-06-30 08:21:45 +0200

    Prevent that sortset is replaced with a not sorted set

--- src/test/java/com/ecpnv/openrewrite/jdo2jpa/ChangeTypesForAnnotatedVariablesTest.java
+++ src/test/java/com/ecpnv/openrewrite/jdo2jpa/ChangeTypesForAnnotatedVariablesTest.java
@@ -1,5 +1,6 @@
 package com.ecpnv.openrewrite.jdo2jpa;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 
@@ -7,77 +8,235 @@ import static org.openrewrite.java.Assertions.java;
 
 class ChangeTypesForAnnotatedVariablesTest extends BaseRewriteTest {
 
-    @DocumentExample
-    @Test
-    void happyPath() {
-        rewriteRun(spec -> spec.parser(PARSER).recipes(new ChangeTypesForAnnotatedVariables(
-                        "javax.persistence.OneToMany",
-                        "java.util.SortedSet,java.util.TreeSet,java.util.Collections.unmodifiableSortedSet",
-                        "java.util.Set,java.util.HashSet,java.util.Collections.unmodifiableSet",
-                        false)),
-                //language=java
-                java(
-                        """
-                                package a;
-                                
-                                import java.util.HashSet;
-                                import java.util.Set;
-                                import java.util.SortedSet;
-                                import java.util.TreeSet;
-                                import java.util.Collections;
-                                
-                                import javax.persistence.CascadeType;
-                                import javax.persistence.OneToMany;
-                                
-                                public class SomeClass {
-                                    @OneToMany(mappedBy = "name", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
-                                    private SortedSet<String> someSet = new TreeSet<>();
-                                
-                                    @OneToMany(mappedBy = "type", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
-                                    private Set<String> someTypeSet = new HashSet<>();
-                                
-                                    private SortedSet<String> someOtherSet = new TreeSet<>();
-                                
-                                    public SortedSet<String> getSomeSet() {
-                                        return Collections.unmodifiableSortedSet(someSet);
+    @Nested
+    class TestChangeTypesForAnnotatedVariablesRecipe extends BaseRewriteTest {
+
+
+        @DocumentExample
+        @Test
+        void happyPathWithMethodChange() {
+            rewriteRun(spec -> spec.parser(PARSER).recipes(new ChangeTypesForAnnotatedVariables(
+                            "javax.persistence.OneToMany",
+                            "java.util.SortedSet,java.util.TreeSet,java.util.Collections.unmodifiableSortedSet",
+                            "java.util.Set,java.util.HashSet,java.util.Collections.unmodifiableSet",
+                            false, false)),
+                    //language=java
+                    java(
+                            """
+                                    package a;
+                                    
+                                    import java.util.HashSet;
+                                    import java.util.Set;
+                                    import java.util.SortedSet;
+                                    import java.util.TreeSet;
+                                    import java.util.Collections;
+                                    
+                                    import javax.persistence.CascadeType;
+                                    import javax.persistence.OneToMany;
+                                    
+                                    public class SomeClass {
+                                        @OneToMany(mappedBy = "name", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private SortedSet<String> someSet = new TreeSet<>();
+                                    
+                                        @OneToMany(mappedBy = "type", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private Set<String> someTypeSet = new HashSet<>();
+                                    
+                                        private SortedSet<String> someOtherSet = new TreeSet<>();
+                                    
+                                        public SortedSet<String> getSomeSet() {
+                                            return Collections.unmodifiableSortedSet(someSet);
+                                        }
+                                    
+                                        public SortedSet<String> getSomeOtherSet() {
+                                            return Collections.unmodifiableSortedSet(someOtherSet);
+                                        }
                                     }
-                                
-                                    public SortedSet<String> getSomeOtherSet() {
-                                        return Collections.unmodifiableSortedSet(someOtherSet);
+                                    """,
+                            """
+                                    package a;
+                                    
+                                    import java.util.HashSet;
+                                    import java.util.Set;
+                                    import java.util.SortedSet;
+                                    import java.util.TreeSet;
+                                    import java.util.Collections;
+                                    
+                                    import javax.persistence.CascadeType;
+                                    import javax.persistence.OneToMany;
+                                    
+                                    public class SomeClass {
+                                        @OneToMany(mappedBy = "name", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private Set<String> someSet = new HashSet<>();
+                                    
+                                        @OneToMany(mappedBy = "type", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private Set<String> someTypeSet = new HashSet<>();
+                                    
+                                        private SortedSet<String> someOtherSet = new TreeSet<>();
+                                    
+                                        public Set<String> getSomeSet() {
+                                            return Collections.unmodifiableSet(someSet);
+                                        }
+                                    
+                                        public SortedSet<String> getSomeOtherSet() {
+                                            return Collections.unmodifiableSortedSet(someOtherSet);
+                                        }
                                     }
-                                }
-                                """,
-                        """
-                                package a;
-                                
-                                import java.util.HashSet;
-                                import java.util.Set;
-                                import java.util.SortedSet;
-                                import java.util.TreeSet;
-                                import java.util.Collections;
-                                
-                                import javax.persistence.CascadeType;
-                                import javax.persistence.OneToMany;
-                                
-                                public class SomeClass {
-                                    @OneToMany(mappedBy = "name", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
-                                    private Set<String> someSet = new HashSet<>();
-                                
-                                    @OneToMany(mappedBy = "type", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
-                                    private Set<String> someTypeSet = new HashSet<>();
-                                
-                                    private SortedSet<String> someOtherSet = new TreeSet<>();
-                                
-                                    public Set<String> getSomeSet() {
-                                        return Collections.unmodifiableSet(someSet);
+                                    """
+                    )
+            );
+        }
+
+        @DocumentExample
+        @Test
+        void happyPathNoMethodChange() {
+            rewriteRun(spec -> spec.parser(PARSER).recipes(new ChangeTypesForAnnotatedVariables(
+                            "javax.persistence.OneToMany",
+                            "java.util.SortedSet,java.util.Collections.unmodifiableSortedSet",
+                            "java.util.TreeSet,java.util.Collections.unmodifiableSet",
+                            false, true)),
+                    //language=java
+                    java(
+                            """
+                                    package a;
+                                    
+                                    import java.util.HashSet;
+                                    import java.util.Set;
+                                    import java.util.SortedSet;
+                                    import java.util.TreeSet;
+                                    import java.util.Collections;
+                                    
+                                    import javax.persistence.CascadeType;
+                                    import javax.persistence.OneToMany;
+                                    
+                                    public class SomeClass {
+                                        @OneToMany(mappedBy = "name", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private SortedSet<String> someSet = new TreeSet<>();
+                                    
+                                        @OneToMany(mappedBy = "type", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private Set<String> someTypeSet = new HashSet<>();
+                                    
+                                        private SortedSet<String> someOtherSet = new TreeSet<>();
+                                    
+                                        public SortedSet<String> getSomeSet() {
+                                            return Collections.unmodifiableSortedSet(someSet);
+                                        }
+                                    
+                                        public SortedSet<String> getSomeOtherSet() {
+                                            return Collections.unmodifiableSortedSet(someOtherSet);
+                                        }
                                     }
-                                
-                                    public SortedSet<String> getSomeOtherSet() {
-                                        return Collections.unmodifiableSortedSet(someOtherSet);
+                                    """,
+                            """
+                                    package a;
+                                    
+                                    import java.util.HashSet;
+                                    import java.util.Set;
+                                    import java.util.SortedSet;
+                                    import java.util.TreeSet;
+                                    import java.util.Collections;
+                                    
+                                    import javax.persistence.CascadeType;
+                                    import javax.persistence.OneToMany;
+                                    
+                                    public class SomeClass {
+                                        @OneToMany(mappedBy = "name", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private TreeSet<String> someSet = new TreeSet<>();
+                                    
+                                        @OneToMany(mappedBy = "type", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private Set<String> someTypeSet = new HashSet<>();
+                                    
+                                        private SortedSet<String> someOtherSet = new TreeSet<>();
+                                    
+                                        public SortedSet<String> getSomeSet() {
+                                            return Collections.unmodifiableSortedSet(someSet);
+                                        }
+                                    
+                                        public SortedSet<String> getSomeOtherSet() {
+                                            return Collections.unmodifiableSortedSet(someOtherSet);
+                                        }
                                     }
-                                }
-                                """
-                )
-        );
+                                    """
+                    )
+            );
+        }
+    }
+
+    @Nested
+    class TestWithAllRecipes extends BaseRewriteTest {
+
+        @DocumentExample
+        @Test
+        void happyPathNoMethodChange() {
+            rewriteRun(spec -> spec.parser(PARSER)
+                            .recipeFromResources("com.ecpnv.openrewrite.jdo2jpa.v2x"),
+                    //language=java
+                    java(
+                            """
+                                    package a;
+                                    
+                                    import java.util.HashSet;
+                                    import java.util.Set;
+                                    import java.util.SortedSet;
+                                    import java.util.TreeSet;
+                                    import java.util.Collections;
+                                    
+                                    import javax.persistence.CascadeType;
+                                    import javax.persistence.Entity;
+                                    import javax.persistence.OneToMany;
+                                    
+                                    @Entity
+                                    public class SomeClass {
+                                        @OneToMany(mappedBy = "name", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private SortedSet<String> someSet = new TreeSet<>();
+                                    
+                                        @OneToMany(mappedBy = "type", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private Set<String> someTypeSet = new HashSet<>();
+                                    
+                                        private SortedSet<String> someOtherSet = new TreeSet<>();
+                                    
+                                        public SortedSet<String> getSomeSet() {
+                                            return Collections.unmodifiableSortedSet(someSet);
+                                        }
+                                    
+                                        public SortedSet<String> getSomeOtherSet() {
+                                            return Collections.unmodifiableSortedSet(someOtherSet);
+                                        }
+                                    }
+                                    """,
+                            """
+                                    package a;
+                                    
+                                    import java.util.HashSet;
+                                    import java.util.Set;
+                                    import java.util.SortedSet;
+                                    import java.util.TreeSet;
+                                    import java.util.Collections;
+                                    
+                                    import javax.persistence.CascadeType;
+                                    import javax.persistence.Entity;
+                                    import javax.persistence.OneToMany;
+                                    
+                                    @Entity
+                                    public class SomeClass {
+                                        @OneToMany(mappedBy = "name", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private TreeSet<String> someSet = new TreeSet<>();
+                                    
+                                        @OneToMany(mappedBy = "type", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.LAZY)
+                                        private Set<String> someTypeSet = new HashSet<>();
+                                    
+                                        private SortedSet<String> someOtherSet = new TreeSet<>();
+                                    
+                                        public SortedSet<String> getSomeSet() {
+                                            return Collections.unmodifiableSortedSet(someSet);
+                                        }
+                                    
+                                        public SortedSet<String> getSomeOtherSet() {
+                                            return Collections.unmodifiableSortedSet(someOtherSet);
+                                        }
+                                    }
+                                    """
+                    )
+            );
+        }
     }
 }
